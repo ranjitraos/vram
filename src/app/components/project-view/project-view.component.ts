5f1cfb8ce7a3773050f1af4fd7108980b8d673df
ranjitraos <38098819+ranjitraos@users.noreply.github.com>
2020-05-24 20:43:25 +0530

    -Removed slider, header and other unwanted codes

--- src/app/components/project-view/project-view.component.ts
+++ src/app/components/project-view/project-view.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router,ActivatedRoute, ParamMap } from '@angular/router';
 
-declare var UnityLoader:any;
-declare var UnityProgress:any;
 
 @Component({
   selector: 'app-project-view',
@@ -11,16 +9,9 @@ declare var UnityProgress:any;
 })
 export class ProjectViewComponent implements OnInit {
 
-  sceneId:any;
-  gameInstance:any;
 
   constructor(private activatedRoute:ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params)=>{
-      this.sceneId=params.id;
-    });
-    this.gameInstance = UnityLoader.instantiate("gameContainer", "../../assets/Unity/"+this.sceneId+"/Build/web gl test.json", { onProgress: UnityProgress });
-  }
+  ngOnInit(): void {}
 
 }
